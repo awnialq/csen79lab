5114eaffe431321f2bbc13a4b4fe56bf0b88acd5
awni alquraini <aalquraini@linux22404.dc.engr.scu.edu>
2025-10-08 14:06:43 -0700

    doing a bit more work before lab

--- lab3aalquraini/roster.cxx
+++ lab3aalquraini/roster.cxx
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <iomanip>
 #include <stdexcept>
+#include <cassert>
 #include "person.h"
 #include "roster.h"
 
@@ -12,15 +13,30 @@
 namespace csen79 {
 
 	// STUDENT WORK
-	Roster::Roster() {}
+	Roster::Roster() {
+		for(int i = 0; i < CAPACITY; i++){
+			roster[i] = Person();
+		}
+		idx = 0;
+		last = 0;
+	}
 	// insert into the roster
 	void Roster::insert(const T &rec) {
-		// STUDENT WORK
+		if(last > CAPACITY){	//Checks if last will be out of bounds. if so throw an error for another function to catch
+			throw ("Index out of bounds error");
+		}
+		roster[last++] = rec;
 	}
 
 	// simply mark the slot empty
 	void Roster::erase(Person::ID_t id) {
-		// STUDENT WORK
+		while(idx < CAPACITY){
+			if(roster[idx].getID() == id){
+				roster[idx] = Person();
+				break;
+			}
+			idx++;
+		}
 	}
 
 	// rudimentary iterator
