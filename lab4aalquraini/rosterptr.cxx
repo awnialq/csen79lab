26c5823146c16b3716e4ad518dfb0f688e0de1b1
awni alquraini <aalquraini@linux22404.dc.engr.scu.edu>
2025-10-15 15:10:19 -0700

    code works, but takes a while to execute

--- lab4aalquraini/rosterptr.cxx
+++ lab4aalquraini/rosterptr.cxx
@@ -10,15 +10,59 @@
 #include "roster.h"
 #include "rosterptr.h"
 
+// if array fills allocate new mem and copy over the array to the larger size
+
+
 namespace csen79 {
 	// write code for all these
-	RosterPtr::RosterPtr() {}
-	RosterPtr::~RosterPtr() {}
-	bool RosterPtr::insert(const T &rec) { return false; }
-	void RosterPtr::erase(Person::ID_t id) { }
-	Roster::T* RosterPtr::begin(void) { return nullptr; }
-	Roster::T* RosterPtr::end(void) { return nullptr; }
-	Roster::T* RosterPtr::next(void) {return nullptr; }
+	RosterPtr::RosterPtr() {
+		capacity = 30;
+		roster = new T[capacity];
+		idx = 0;
+		last = 0;
+	}
+	RosterPtr::~RosterPtr() {
+		delete [] roster;
+	}
+	bool RosterPtr::insert(const T &rec) { 
+		if(last >= 30){
+			capacity = capacity * 2;
+			
+			T *temp = new T[capacity];
+			for(auto i = 0; i < last; i++){
+				temp[i] = roster[i];
+			}
+			delete [] roster;
+			roster = temp;
+		}
+		roster[last++] = rec;
+		return true;
+	 }
+	void RosterPtr::erase(Person::ID_t id) {
+		bool isFound = false;
+
+		for(auto i = 0; i < last; i++){
+			if(roster[i].getID() == id){
+				for(auto j = i; j < last - 1; j++){
+					roster[j] = roster[j+1];
+				}
+				last--;
+				isFound = true;
+			}
+		}
+
+		if(!isFound) {throw std::invalid_argument("ID does not exist in the roster");}
+	}
+	Roster::T* RosterPtr::begin(void) { 
+		idx = 0;
+		return &(roster[idx++]);
+	}
+	Roster::T* RosterPtr::end(void) { 
+		return &(roster[last]);	
+	}
+	Roster::T* RosterPtr::next(void) { 
+		return &(roster[++idx]);
+	}
 
 
 }	// namespace
