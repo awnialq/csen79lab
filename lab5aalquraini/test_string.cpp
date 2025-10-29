016d1c65e6f01e4f097f07375cac5cadafba8a43
awni alquraini <aalquraini@linux10622.dc.engr.scu.edu>
2025-10-29 12:42:33 -0700

    implemented most of the stuff

--- lab5aalquraini/test_string.cpp
+++ lab5aalquraini/test_string.cpp
@@ -1,6 +1,15 @@
 #include "mystring.h"
 #include <iostream>
 
+using namespace coen79_lab5
+
 int main(){
+    string default_str();
+    string char_str('A');
+
+    default_str += char_str;
+    std::cout << default_str << std::endl;
+
+
     return EXIT_SUCCESS;
 }
