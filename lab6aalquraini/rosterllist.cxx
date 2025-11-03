a2567b585858d765f5feed91ca17c2ed17bb1849
awni alquraini <aalquraini@linux10620.dc.engr.scu.edu>
2025-11-02 22:26:44 -0800

    first attempt at implementation

--- lab6aalquraini/rosterllist.cxx
+++ lab6aalquraini/rosterllist.cxx
@@ -13,18 +13,80 @@ namespace csen79 {
 	// STUDENT WORKS
 	// IMPLEMENT ALL THESE FUNCTIONS
 	// These are code only to make the program compile.  They do not work!
-	RosterLList::RosterLList() {}
-	RosterLList::~RosterLList() {}
-	RosterLList::Node::Node(){}
-	RosterLList::Node::Node(T &t){}
-	RosterLList::Node::~Node(){}
-	RosterLList::iterator RosterLList::begin(){return RosterLList::iterator(nullptr); }
-	RosterLList::iterator RosterLList::end(){return RosterLList::iterator(nullptr); }
-	bool RosterLList::insert(T &rec) {return false;}
-	void RosterLList::erase(Person::ID_t id) {}
+	RosterLList::RosterLList() {
+		head = nullptr;
+	}
+	RosterLList::~RosterLList() {
+		if(head != nullptr){ delete head;}
+	}
 
-	RosterLList::iterator::iterator(Node *t) {}
-	bool RosterLList::iterator::operator !=(const iterator &rhs) {return false;}
+	RosterLList::Node::Node(){
+		next = nullptr;
+	}
+	RosterLList::Node::Node(T &t){
+		data = t;
+		next = nullptr;
+	}
+	RosterLList::Node::~Node(){
+		if(next != nullptr){
+			delete next;
+		}
+	}
+
+
+	RosterLList::iterator RosterLList::begin(){
+		return RosterLList::iterator(head);
+	}
+
+	RosterLList::iterator RosterLList::end(){
+		return RosterLList::iterator(nullptr);
+	}
+
+	bool RosterLList::insert(T &rec) {
+		if(head == nullptr){
+			head = new Node(rec);
+			head->next = nullptr;
+			return true;
+		}
+		Node *n = head;
+
+		while(n->next != nullptr){
+			n = n->next;
+		}
+
+		n->next = new Node(rec);
+		return true;
+	}
+
+	void RosterLList::erase(Person::ID_t id) {
+		if(head == nullptr){
+			return;
+		}
+		if(head->data.getID() == id){
+			delete head;
+			head = head->next;
+		}
+		Node *n = head;
+		while(n->next != nullptr){
+			if(n->next->data.getID() == id){
+				Node *temp = n->next->next;
+				delete n->next;
+				n->next = temp;
+				break;
+			}
+			n = n->next;
+		}
+	}
+
+	RosterLList::iterator::iterator(Node *t) {
+		ptr = t;
+	}
+	bool RosterLList::iterator::operator !=(const iterator &rhs) {
+		return !(this->ptr == rhs.ptr);
+	}
 	Roster::T &RosterLList::iterator::operator *() { return ptr->data; }
-	RosterLList::iterator &RosterLList::iterator::operator++() {return *this; }
+	RosterLList::iterator &RosterLList::iterator::operator++() {
+		ptr = ptr->next;
+		return *this; 
+	}
 }
