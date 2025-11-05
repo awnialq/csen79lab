2f9bf6a577e9c0cc374bdac03a3a907d594912c9
awni alquraini <aalquraini@linux20308.dc.engr.scu.edu>
2025-11-05 10:55:51 -0800

    basically final

--- lab6aalquraini/rosterllist.cxx
+++ lab6aalquraini/rosterllist.cxx
@@ -14,10 +14,10 @@ namespace csen79 {
 	// IMPLEMENT ALL THESE FUNCTIONS
 	// These are code only to make the program compile.  They do not work!
 	RosterLList::RosterLList() {
-		head = nullptr;
+		head = nullptr;	//set the head to nullptr as there is nothing in the list yet
 	}
 	RosterLList::~RosterLList() {
-		while(head != nullptr){
+		while(head != nullptr){	//keep on deleting until the end of the linked list
 			Node *temp = head;
 			head = head->next;
 			delete temp; 
@@ -25,49 +25,52 @@ namespace csen79 {
 	}
 
 	RosterLList::Node::Node(){
-		next = nullptr;
+		next = nullptr;	//set the nest variable in the node to nullptr. no data to assign
 	}
 	RosterLList::Node::Node(T &t){
-		data = t;
-		next = nullptr;
+		data = t;	// set the data to the passed student
+		next = nullptr;	//set the next variable to nullptr
 	}
-	RosterLList::Node::~Node(){}
+	RosterLList::Node::~Node(){}	// no memory allocation required so no need to define a body
 
 
 	RosterLList::iterator RosterLList::begin(){
-		return RosterLList::iterator(head);
+		return RosterLList::iterator(head);	//return an iterator with head as its internal pointer
 	}
 
 	RosterLList::iterator RosterLList::end(){
-		return RosterLList::iterator(nullptr);
+		return RosterLList::iterator(nullptr);	//return nullptr as that is the end of the linked list
 	}
 
 	bool RosterLList::insert(T &rec) {
-		if(head == nullptr){
-			head = new Node(rec);
+		if(head == nullptr){	// check if head is null
+			head = new Node(rec);	///if it is then insert there
 			head->next = nullptr;
 			return true;
 		}
-		Node *n = head;
+		Node *n = head;	//set a temp variable to hand iteration
 
-		while(n->next != nullptr){
+		while(n->next != nullptr){	// while you are not at the end of the linked list, go to the next node
 			n = n->next;
 		}
 
-		n->next = new Node(rec);
+		n->next = new Node(rec);	//set the next (which should be nullptr) to a new Node with the passed record.
 		return true;
 	}
 
 	void RosterLList::erase(Person::ID_t id) {
-		if (head == nullptr) {return;}
+		if (head == nullptr) {return;}	//if head is null id can't be there so just leave
 
-		if(head->data.getID() == id){
+		if(head->data.getID() == id){	//if the head has the ID, we have special code to handle it
 			Node *temp = head;
 			head = head->next;
 			delete temp;
-			return;
-		}
+			return; // leave when done
+		}	
 
+		/*
+		*	This logic handles deletion when the head is not the ID
+		*/
 
 		Node *n = head;
 		while(n != nullptr && n->next != nullptr){
@@ -75,21 +78,21 @@ namespace csen79 {
 				Node *temp = n->next;
 				n->next = n->next->next;
 				delete temp;
-				break;
+				return;
 			}
 			n = n->next;
 		}
 	}
 
-	RosterLList::iterator::iterator(Node *t) {
+	RosterLList::iterator::iterator(Node *t) {	//very trivial iterator constructor
 		ptr = t;
 	}
-	bool RosterLList::iterator::operator !=(const iterator &rhs) {
+	bool RosterLList::iterator::operator !=(const iterator &rhs) {	//compares two iterator types
 		return !(this->ptr == rhs.ptr);
 	}
-	Roster::T &RosterLList::iterator::operator *() { return ptr->data; }
+	Roster::T &RosterLList::iterator::operator *() { return ptr->data; }	//returns the data inside the internal pointer using the dereference
 	RosterLList::iterator &RosterLList::iterator::operator++() {
-		ptr = ptr->next;
-		return *this; 
+		ptr = ptr->next;	//go to the next node in the linked list
+		return *this; //return the object itself.
 	}
 }
--- lab6aalquraini/rosterllistmain.cxx
+++ lab6aalquraini/rosterllistmain.cxx
@@ -2,7 +2,12 @@
  * CSEN 79 Lab 6 - Main Tester
  * Instructions on how to write the tester class:
  * 	1. In order to write this tester, you must utilize the commands defined in roster.h (Erase = 'X', Add = 'A', Print = 'L')
- *	2. 
+ *	2. Read from an input stream (std::cin) the commands and branch accordingly
+ *	3. For 'A' command, read a Person object from the input stream and insert it into the RosterLList object
+ *	4. For 'X' command, read a Person::ID_t from the input stream and erase the corresponding Person object from the RosterLList object
+ *	5. For 'L' command, print the entire roster using the iterator defined in RosterLList
+ *  6. Handle exceptions where necessary
+ * 	7. Edgecases should be tested in the lab3sampleinput.txt file (Removing head)
  */
 #include <string>
 #include <iostream>
